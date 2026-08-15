6ffcd5195e00d98e61fa521b0b18e75951fb14ea
otaviobarretojr <85048335+otaviobarretojr@users.noreply.github.com>
2026-08-14 20:53:12 -0400

    fix: precache compact exercise images in v11

--- v11/sw.js
+++ v11/sw.js
@@ -1,5 +1,5 @@
 const CACHE='nielle-fit-v11-20260815-pwa';
-const ASSETS=["./", "./index.html", "./manifest.webmanifest", "./icon-192.svg", "./icon-512.svg", "./sprite/0.txt", "./sprite/1.txt", "./sprite/2.txt", "./sprite/3.txt", "./sprite/4.txt", "./sprite/5.txt", "./sprite/6.txt", "./sprite/7.txt", "./sprite/8.txt", "./sprite/9.txt", "./sprite/10.txt", "./sprite/11.txt"];
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-192.svg","./icon-512.svg","./sprite-tiny.txt"];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match('./index.html'))));});
